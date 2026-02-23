dddad82840745e3d4c84339619fd13ec100ebcec
Kartikey Tiwari <123266503+Codewallahh@users.noreply.github.com>
2026-02-23 11:15:55 +0530

    Update install_server_tools.sh

--- install_server_tools.sh
+++ install_server_tools.sh
@@ -58,16 +58,11 @@ java -version
 # -------------------------
 echo "🤖 Installing Jenkins..."
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | \
-sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+wget https://pkg.jenkins.io/debian-stable/binary/jenkins_2.541.1_all.deb
 
-echo \
-"deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/" | \
-sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo dpkg -i jenkins_2.541.1_all.deb
 
-sudo apt update -y
-sudo apt install -y jenkins
+sudo apt -f install -y
 
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
@@ -76,7 +71,7 @@ sudo systemctl start jenkins
 # Allow Jenkins to use Docker
 sudo usermod -aG docker jenkins
 sudo systemctl restart jenkins
-
+docker newgrp
 # -------------------------
 # Final Status Check
 # -------------------------
